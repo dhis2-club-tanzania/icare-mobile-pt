f3ae0fbe87caba01a1e59fecfb867e7e2020ba06
mwakyusa <josephatjulius24@gmail.com>
2022-01-14 16:43:04 +0300

    remove drawer

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -22,7 +22,6 @@ class _HomePageState extends State<HomePage> {
   String currentTitle = 'Stock Taking';
   String currentModuleId = 'stocktaking';
   LocationModel currentLocation = LocationModel(uuid: '', display: '');
-  Widget bodyContainer = Text('data');
   @override
   Widget build(BuildContext context) {
     List<PopupMenuEntry<LocationModel>> moreMenuItems = [];
@@ -34,64 +33,49 @@ class _HomePageState extends State<HomePage> {
         )
       ]
     });
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(currentTitle),
-        actions: [
-          Container(
-            child: PopupMenuButton(
-              icon: Icon(Icons.more_vert),
-              onSelected: (LocationModel result) { setState(() { currentLocation = result; }); },
-              itemBuilder: (BuildContext context) => moreMenuItems,
-            ),
-          )
-        ],
-      ),
-      body: Container(
-          child: currentLocation.uuid != '' ? StockTakingPage(authToken: widget.authToken, baseUrl: widget.baseUrl, locationDetails: currentLocation):
-            Column(
-              children: [
-                SizedBox(
-                  height: 30,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(tabs: [
+              Tab(icon: Icon(Icons.storage),),
+              Tab(icon: Icon(Icons.countertops)),
+            ]),
+            title: Text(currentTitle),
+            actions: [
+              Container(
+                child: PopupMenuButton(
+                  icon: Icon(Icons.more_vert),
+                  onSelected: (LocationModel result) { setState(() { currentLocation = result; }); },
+                  itemBuilder: (BuildContext context) => moreMenuItems,
                 ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text('Select Location first', textAlign: TextAlign.center, style: TextStyle(fontSize: 15)),
-                )
-              ],
-            ),
-      ),
-       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Container(
-              height: 130,
-              child: DrawerHeader(
-                child: Text('Menu'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+              )
+            ],
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.storage),
+              Container(
+                child: currentLocation.uuid != '' ? StockTakingPage(authToken: widget.authToken, baseUrl: widget.baseUrl, locationDetails: currentLocation):
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: Text('Select Location first', textAlign: TextAlign.center, style: TextStyle(fontSize: 15)),
+                    )
+                  ],
                 ),
-              ),
-            ),
-            ListTile(
-              title: Text('Stock Taking'),
-              onTap: () {
-                setState(() {
-                  currentTitle = 'Stock taking';
-                  currentModuleId = 'stocktaking';
-                });
-                Navigator.pop(context);
-              },
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
 
